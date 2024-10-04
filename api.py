6218d6ac0394b8a1f3ac9662c169b1690ef17233
christianjensen2903 <cmj290300@gmail.com>
2024-10-04 02:00:02 +0200

    ✨ request converter

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import datetime
 import time
 from loguru import logger
@@ -9,7 +9,9 @@ from sim.dtos import (
     TrafficSimulationPredictRequestDto,
     SignalDto,
 )
+from typing import Annotated
 import json
+from api_env import RequestConverter
 
 HOST = "0.0.0.0"
 PORT = 9051
@@ -31,68 +33,26 @@ def index():
     return "Your endpoint is running!"
 
 
+converter = RequestConverter()
+
+
+def get_converter() -> RequestConverter:
+    return converter
+
+
 @app.post("/predict", response_model=TrafficSimulationPredictResponseDto)
-def predict_endpoint(request: TrafficSimulationPredictRequestDto):
+def predict_endpoint(
+    request: TrafficSimulationPredictRequestDto,
+    # converter: Annotated[RequestConverter, Depends(get_converter)],
+):
 
-    # Decode request
-    data = request
-    vehicles = data.vehicles
-    total_score = data.total_score
-    simulation_ticks = data.simulation_ticks
-    signals = data.signals
-    signal_groups = data.signal_groups
-    legs = data.legs
-    allowed_green_signal_combinations = data.allowed_green_signal_combinations
-    is_terminated = data.is_terminated
-
-    if simulation_ticks == 30:
-        with open(f"vehicles_per_hour.json", "r+") as f:
-            vehicles_per_hour_data = json.load(f)
-            sim2 = "A1RightTurn" in signal_groups
-            grp = "2" if sim2 else "1"
-            for leg in legs:
-                vehicle_count = len(
-                    [vehicle for vehicle in vehicles if vehicle.leg == leg.name]
-                )
-                if leg.name not in vehicles_per_hour_data[grp]:
-                    vehicles_per_hour_data[grp][leg.name] = []
-                vehicles_per_hour_data[grp][leg.name].append(vehicle_count / 30 * 3600)
-            f.seek(0)
-            f.truncate()
-            json.dump(vehicles_per_hour_data, f)
-
-    # # Read the vehicles
-    # with open(f"threshold.json", "r") as f:
-    #     threshold = json.load(f)
-
-    # threshold[simulation_ticks] = [
-    #     vehicle.model_dump(mode="json") for vehicle in vehicles
-    # ]
-
-    # with open(f"threshold.json", "w") as f:
-    #     json.dump(threshold, f)
-
-    logger.info(f"Number of vehicles at tick {simulation_ticks}: {len(vehicles)}")
-
-    # Select a signal group to go green
-    # green_signal_group = signal_groups[0]
-
-    # if simulation_ticks > 100 and simulation_ticks < 120:
-    #     signals = [
-    #         SignalDto(name="A1", state="green"),
-    #         SignalDto(name="A2", state="green"),
-    #     ]
-    # elif simulation_ticks > 120 and simulation_ticks < 150:
-    #     signals = [SignalDto(name="A1", state="red"), SignalDto(name="A2", state="red")]
-    # elif simulation_ticks > 150 and simulation_ticks < 170:
-    #     signals = [SignalDto(name="B1", state="green")]
-    # elif simulation_ticks > 170 and simulation_ticks < 190:
-    #     signals = [SignalDto(name="B1", state="red")]
-    # elif simulation_ticks > 190 and simulation_ticks < 210:
-    #     signals = [SignalDto(name="B2", state="green")]
+    obs = converter.convert_vehicles(request.vehicles)
+    # print(obs)/
+    if request.simulation_ticks < 20:
+        print(request.simulation_ticks)
 
     # Return the encoded image to the validation/evalution service
-    response = TrafficSimulationPredictResponseDto(signals=signals)
+    response = TrafficSimulationPredictResponseDto(signals=[])
 
     return response
 
---
+++ api_env.py
@@ -0,0 +1,155 @@
+from typing import Literal
+from pydantic import BaseModel
+import numpy as np
+from generate_road import LaneType, index_to_direction
+from sumo_env import TrafficColor, SignalState
+from sim.dtos import VehicleDto, SignalDto, LegDto, TrafficSimulationPredictRequestDto
+
+
+class RequestConverter:
+
+    def __init__(self):
+        self.amber_time = 4
+        self.red_amber_time = 2
+        self.min_green_time = 6
+        self.signal_states: dict[str, SignalState] = {}
+
+    def convert_leg_naming(leg_name: str) -> str:
+        if leg_name == "A1":
+            return "S"
+        elif leg_name == "A2":
+            return "N"
+        elif leg_name == "B2":
+            return "W"
+        elif leg_name == "B1":
+            return "E"
+
+    def convert_vehicles(self, vehicles: list[VehicleDto]) -> dict[str, list[dict]]:
+        converted_vehicles: dict[str, list[dict]] = {}
+        for vehicle in vehicles:
+            leg_name = self.convert_leg_naming(vehicle.leg)
+            if leg_name not in converted_vehicles:
+                converted_vehicles[leg_name] = []
+
+            converted_vehicles[leg_name].append(
+                {"speed": vehicle.speed, "distance_to_stop": vehicle.distance_to_stop}
+            )
+
+        return converted_vehicles
+
+    def convert_color(self, color: str) -> TrafficColor:
+        if color == "red":
+            return TrafficColor.RED
+        elif color == "amber":
+            return TrafficColor.AMBER
+        elif color == "redamber":
+            return TrafficColor.REDAMBER
+        elif color == "green":
+            return TrafficColor
+
+    def initialize_signals(self, legs: dict[str, list[LaneType]]) -> None:
+        for leg_name, lanes in legs.items():
+            for lane in lanes:
+                group_name = f"{leg_name}_1_{lane.value}"
+                self.signal_states[group_name] = SignalState()
+
+    def convert_legs(self, leg: LegDto) -> dict[str, list[LaneType]]:
+        lanes: dict[str, list[LaneType]] = {}
+        leg_name = self.convert_leg_naming(leg.name)
+        for lane in leg.lanes:
+            if leg_name not in lanes:
+                lanes[leg_name] = []
+            if lane == "Left":
+                lanes[leg_name].append(LaneType.LEFT)
+            elif lane == "Right":
+                lanes[leg_name].append(LaneType.RIGHT)
+            elif lane == "Main":
+                if leg.name in leg.signal_groups:
+                    lanes[leg_name].append(LaneType.STRAIGHT)
+                else:  # A bit hacky but works for this usecase
+                    lanes[leg_name].append(LaneType.STRAIGHT_LEFT)
+
+        return lanes
+
+    def convert_signals(self, signals: list[SignalDto]) -> dict[str, str]:
+        converted_signals: dict[str, str] = {}
+        for signal in signals:
+            converted_signals[signal.name] = signal.state
+
+        return converted_signals
+
+    def get_signals(self) -> dict[str, dict]:
+        signals: dict[str, list[str]] = {}
+        for group_name, signal in self.signal_states.items():
+            signals[group_name] = {
+                "color": signal.color.value,
+                "time": min(signal.time, 6),
+            }
+
+        return signals
+
+    def _update_traffic_lights(
+        self, action: dict[str, Literal[TrafficColor.GREEN, TrafficColor.RED]]
+    ):
+        """Update the traffic lights based on the action"""
+        for group, state in self.signal_states.items():
+            desired_color = action.get(group, TrafficColor.RED)
+
+            if (
+                state.color == TrafficColor.GREEN
+                and desired_color == TrafficColor.RED
+                and state.time >= self.min_green_time
+            ):
+                state.color = TrafficColor.AMBER
+                state.time = 0
+            elif state.color == TrafficColor.AMBER and state.time >= self.amber_time:
+                state.color = TrafficColor.RED
+                state.time = 0
+            elif (
+                state.color == TrafficColor.REDAMBER
+                and state.time >= self.red_amber_time
+            ):
+                state.color = TrafficColor.GREEN
+                state.time = 0
+            elif (
+                state.color == TrafficColor.RED and desired_color == TrafficColor.GREEN
+            ):
+                state.color = TrafficColor.REDAMBER
+                state.time = 0
+            else:  # Not ready to shift or not legal change
+                state.time += 1
+
+    def _parse_action(
+        self, action: np.ndarray
+    ) -> dict[str, Literal[TrafficColor.GREEN, TrafficColor.RED]]:
+        """Parse the action into a dictionary"""
+        action_dict = {}
+        for i, a in enumerate(action):
+
+            direction_index = i // len(LaneType)
+            lane_index = i % len(LaneType)
+            direction = index_to_direction(direction_index)
+            lane_type = list(LaneType)[lane_index]
+            group = f"{direction.value}_{lane_type.value}"
+            action_dict[group] = TrafficColor.GREEN if a else TrafficColor.RED
+
+        return action_dict
+
+    def update_signals(self, action: np.ndarray) -> None:
+        parsed_action = self._parse_action(action)
+        self._update_traffic_lights(parsed_action)
+
+    def convert_request(self, request: TrafficSimulationPredictRequestDto) -> dict:
+        vehicles = self.convert_vehicles(request.vehicles)
+        legs = self.convert_legs(legs)
+
+        if self.signal_states == {}:
+            self.initialize_signals(legs)
+
+        signals = self.convert_signals(signals)
+
+        return {
+            "vehicles": vehicles,
+            "signals": signals,
+            "legs": legs,
+        }
